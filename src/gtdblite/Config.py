47948b08ff03a532d9ceb28241211ab785c8ec2e
Askars <a.skarshewski@uq.edu.au>
2015-03-18 14:10:11 +1000

    Creating config file.

--- src/gtdblite/Config.py
+++ src/gtdblite/Config.py
@@ -1,6 +1,5 @@
 GTDB_HOST="host"
 GTDB_USERNAME="dbuser"
 GTDB_DB_NAME="database_name"
-GTDB_DEV_DB_NAME="dev_database_name"
 GTDB_PASSWORD="database_password"
-
+GTDB_GENOME_COPY_DIR="/path/to/copied_genomes/"
